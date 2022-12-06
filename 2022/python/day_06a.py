cef3e6feb7df14cc8f40a55fbf044856be0e47d0
Vladimir Shargin <vladimir.shargin@lingarogroup.com>
2022-12-06 18:21:38 +0100

    Update 2022 day 6

--- 2022/python/day_06a.py
+++ 2022/python/day_06a.py
@@ -1,8 +1,8 @@
 with open("inputs/day_06.txt") as input_file:
     signal = input_file.read()
 
-for i in range(len(signal) - 14):
-    test_string = signal[i:i + 14]
+for i in range(len(signal) - 4):
+    test_string = signal[i:i + 4]
     if len(set(test_string)) == len(test_string):
-        print(i + 14)
+        print(i + 4)
         break
