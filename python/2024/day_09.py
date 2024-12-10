146c23a2ab73e73431228bb1b27bcffbfb36035c
Vladimir Shargin <me@vshargin.ru>
2024-12-10 02:03:30 +0100

    Update 2024 day 9

--- python/2024/day_09.py
+++ python/2024/day_09.py
@@ -6,13 +6,10 @@ class Solution(AOCSolution):
         self._disk_map = puzzle_input[0]
 
     def _solve_common(self):
-        self._file_sizes = [
-            int(file_size)
-            for i, file_size in enumerate(self._disk_map)
-            if int(i) % 2 == 0
-        ]
+        pass
 
-        self._sequence = [
+    def solve_part_1(self):
+        sequence = [
             file_id
             for file_ids in [
                 [i // 2] * int(length) if i % 2 == 0 else [None] * int(length)
@@ -20,65 +17,54 @@ class Solution(AOCSolution):
             ]
             for file_id in file_ids
         ]
-
-    def solve_part_1(self):
-        defragmented_sequence = self._sequence.copy()
-
-        while None in defragmented_sequence:
-            if defragmented_sequence[-1] is None:
-                defragmented_sequence.pop()
+        while None in sequence:
+            if sequence[-1] is None:
+                sequence.pop()
             else:
-                defragmented_sequence[defragmented_sequence.index(None)] = (
-                    defragmented_sequence.pop()
-                )
+                sequence[sequence.index(None)] = sequence.pop()
 
         checksum = 0
-        for i, file_id in enumerate(defragmented_sequence):
+        for i, file_id in enumerate(sequence):
             checksum += i * file_id
 
         return checksum
 
     def solve_part_2(self):
+        cursor = 0
+        files = {}
         gaps = {}
-        i = 0
-        while i < len(self._sequence):
-            if self._sequence[i] is None:
-                start = i
-                while self._sequence[i] is None:
-                    i += 1
-                gaps[start] = i - start
+        for i, size in enumerate(self._disk_map):
+            if int(i) % 2 == 0:
+                files[int(i) // 2] = [cursor, int(size)]
             else:
-                i += 1
+                gaps[cursor] = int(size)
+            cursor += int(size)
 
-        file_starts = [
-            self._sequence.index(file_id) for file_id in range(len(self._file_sizes))
-        ]
-
-        for file_id in reversed(range(0, len(self._file_sizes))):
+        for file_id in reversed(range(0, len(files))):
             candidate_gaps = [
                 start_position
                 for start_position, gap_length in gaps.items()
-                if gap_length >= self._file_sizes[file_id]
-                and start_position < file_starts[file_id]
+                if gap_length >= files[file_id][1]
+                and start_position < files[file_id][0]
             ]
             if not candidate_gaps:
                 continue
             else:
                 new_file_start = min(candidate_gaps)
-                gaps[new_file_start + self._file_sizes[file_id]] = (
-                    gaps[new_file_start] - self._file_sizes[file_id]
+                gaps[new_file_start + files[file_id][1]] = (
+                    gaps[new_file_start] - files[file_id][1]
                 )
                 del gaps[new_file_start]
-                file_starts[file_id] = new_file_start
+                files[file_id][0] = new_file_start
 
         checksum = 0
-        for file_id in range(len(self._file_sizes)):
+        for file_id, file_info in files.items():
             checksum += sum(
                 [
                     file_id * position
                     for position in range(
-                        file_starts[file_id],
-                        file_starts[file_id] + self._file_sizes[file_id],
+                        files[file_id][0],
+                        files[file_id][0] + files[file_id][1],
                     )
                 ]
             )
